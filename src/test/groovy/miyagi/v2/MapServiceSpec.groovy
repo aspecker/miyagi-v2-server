446990f9ab4185e203258c7c24a06c083790dd71
aspecker <specker@gmail.com>
2020-10-19 13:24:53 -0400

    add more unit tests for MapService

--- src/test/groovy/miyagi/v2/MapServiceSpec.groovy
+++ src/test/groovy/miyagi/v2/MapServiceSpec.groovy
@@ -4,7 +4,7 @@ import grails.testing.services.ServiceUnitTest
 import spock.lang.Specification
 
 class MapServiceSpec extends Specification implements ServiceUnitTest<MapService>{
-    def MapService;
+    MapService mapService = new MapService()
     def setup() {
     }
 
@@ -17,11 +17,40 @@ class MapServiceSpec extends Specification implements ServiceUnitTest<MapService
     void "parses punctuation out of string"() {
         given:
             String s = "H!@e#%l^&*l*()o,."
-            String expected = "Hello"
-            MapService mapService = new MapService()
+            String expected = "Helloooo"
         when:
             def parsedString = mapService.parseQueryString(s)
         then:
             parsedString == expected
+            printf("Parses punctuation test succeeded \n")
+    }
+
+    void "replaces spaces with +"() {
+        given:
+            String s = "123 Fake St Funkytown USA 12345"
+            String expected = "123+Fake+St+Funkytown+USA+12345"
+        when:
+            def parseString = mapService.parseQueryString(s)
+        then:
+            parseString == expected
+            printf("Parses spaces test succeeded \n")
+    }
+
+    void "gets results when using a valid address"() {
+        given:
+            String s = "1661 Pennsylvania Avenue NW, Washington, DC 20006"
+        when:
+            String mapResponse = mapService.calculateDistanceToGSHQ(s)
+        then:
+            mapResponse.contains("mi")
+    }
+    void "returns 404 when given a bad address"() {
+        given:
+            String s ="TOTALLYNOTREALADDRESSTOGETBADREQUEST"
+        when:
+            def mapResponse = mapService.calculateDistanceToGSHQ(s)
+        then:
+            mapResponse == 404
+
     }
 }
